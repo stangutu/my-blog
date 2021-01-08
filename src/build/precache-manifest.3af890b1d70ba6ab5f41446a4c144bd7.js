a9041342be45c3d781d16eb8b8c8830102da7821
Sankar Tanguturi <sankara.tanguturi@gmail.com>
2021-01-08 11:45:09 -0500

    First commit

---
+++ src/build/precache-manifest.3af890b1d70ba6ab5f41446a4c144bd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "379f1f4df5edfe195ba8",
+    "url": "/static/css/main.caef32dd.chunk.css"
+  },
+  {
+    "revision": "379f1f4df5edfe195ba8",
+    "url": "/static/js/main.821b1fb4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4d1cdf481cc3938cf395",
+    "url": "/static/js/2.63275db0.chunk.js"
+  },
+  {
+    "revision": "1d33f803d5b63ea9863ac44ee98093d4",
+    "url": "/index.html"
+  }
+];
